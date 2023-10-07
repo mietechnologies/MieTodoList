a0f3f325246881d15f811149a3a974be95504411
Brett Chapin <brett.chapin89@gmail.com>
2023-10-07 17:51:29 -0400

    Initial Commit

---
+++ MIETodoList/MIETodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIETodoListApp.swift
+//  MIETodoList
+//
+//  Created by Brett Chapin on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MIETodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
